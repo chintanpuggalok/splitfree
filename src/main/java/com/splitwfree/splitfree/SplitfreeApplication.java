f88b7f48e3939e6c9a89524e5516408ec31412d7
Chintan Puggalok <chintan19239@iiitd.ac.in>
2024-01-13 13:02:04 +0530

    feat: first commit

---
+++ src/main/java/com/splitwfree/splitfree/SplitfreeApplication.java
@@ -0,0 +1,13 @@
+package com.splitwfree.splitfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitfreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitfreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/splitwfree/splitfree/SplitfreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitwfree.splitfree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitfreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
